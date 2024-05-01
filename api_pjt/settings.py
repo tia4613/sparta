65182e0202c7b3cfb4f3f20ab74f91f0c829e24f
tia <duqrl3000@gmail.com>
2024-05-01 21:13:04 +0900

    seed 생성

--- api_pjt/settings.py
+++ api_pjt/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third_party
+    'django_seed',
+    # Local
     'articles',
+
 ]
 
 MIDDLEWARE = [
