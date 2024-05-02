9e978f402ac238fa9b4df8dca5fe32ff8ab35643
tia <duqrl3000@gmail.com>
2024-05-03 03:06:06 +0900

    JWT 의 유효기간 설정

--- api_pjt/settings.py
+++ api_pjt/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     # Third_party
     'django_seed',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     # Local
     'articles',
     'accounts',
@@ -93,6 +95,13 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
 }
 
 # Password validation
