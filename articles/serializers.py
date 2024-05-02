df3138aa09d158e5af8967ddd24379f40503056f
tia <duqrl3000@gmail.com>
2024-05-02 22:41:50 +0900

    글상세에서 댓글 보기

--- articles/serializers.py
+++ articles/serializers.py
@@ -2,13 +2,16 @@ from rest_framework import serializers
 from .models import Article, Comment
 
 
-class ArticleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = "__all__"
-
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = "__all__"
         read_only_fields = ("article",)
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Article
+        fields = "__all__"
